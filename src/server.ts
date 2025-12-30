db2f6186e3bb3c32650c5f4a97e62b1c8cb24f80
Michiel-ve <michiel.van.eynde123@gmail.com>
2025-12-30 22:42:22 +0200

    fix aikido issue

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,13 @@ import {
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
+import helmet from 'helmet';
 import { join } from 'node:path';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
+app.use(helmet());
 const angularApp = new AngularNodeAppEngine();
 
 /**
