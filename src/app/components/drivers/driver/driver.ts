478a73bff9f9aaf34e750696034da13d297e2ff5
Michiel-ve <michiel.van.eynde123@gmail.com>
2026-03-26 20:12:33 +0200

    refactor: modularize driver and team components, enhance layout and styling

---
+++ src/app/components/drivers/driver/driver.ts
@@ -0,0 +1,33 @@
+import { Component, input } from '@angular/core';
+import { Driver as DriverI } from '../../../interfaces/driver';
+import { TeamInfo } from '../../../interfaces/team-info';
+import { AgePipe } from '../../../pipe/age/age-pipe';
+import { DatePipe } from '@angular/common';
+import { getPodiumGradient } from '../../common/border-accent/border-accent';
+
+@Component({
+  selector: 'app-driver',
+  imports: [AgePipe, DatePipe],
+  templateUrl: './driver.html',
+  styleUrl: './driver.css',
+})
+export class Driver {
+  driver = input.required<DriverI>();
+  position = input<number>(0);
+
+  getTeamInfo(driver: DriverI): TeamInfo {
+    const team = driver.teamSeasons?.[0];
+    return {
+      name: team?.teamName ?? 'Unknown Team',
+      shortName: team?.shortName ?? 'N/A',
+    };
+  }
+
+  getPoints(driver: DriverI): number {
+    return driver.teamSeasons?.[0]?.points ?? 0;
+  }
+
+  borderAccent(pos: number) {
+    return getPodiumGradient(pos);
+  }
+}
